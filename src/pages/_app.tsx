79f6d606524c3c52ed37ed48ec46b41b3f4de642
valdo0 <83303935+valdo0@users.noreply.github.com>
2024-11-25 12:55:07 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
